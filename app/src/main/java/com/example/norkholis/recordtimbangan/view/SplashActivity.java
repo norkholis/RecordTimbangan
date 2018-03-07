6ddcc4fff93b02f53c5263225e920df90275e00a
norkholis <kholis.official@gmail.com>
2018-03-07 15:26:58 +0700

    initial commit

---
+++ app/src/main/java/com/example/norkholis/recordtimbangan/view/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.example.norkholis.recordtimbangan.view;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.norkholis.recordtimbangan.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, LoginPanelActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 500);
+    }
+}
